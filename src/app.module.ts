182ae7dc9596a56d26fbb896b5c8b54e54e6a941
Abdullah Younus <abdullah1001k@gmail.com>
2024-12-09 21:34:03 +0500

    local changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { ErrorLoggingFilter } from './common/exception-filters/error-logging.fil
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerInterceptor } from './common/interceptors/logger.Interceptor';
 import { ConfigModule } from '@nestjs/config';
+import { UtilsModule } from './utils/utils.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true, expandVariables: true }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    UtilsModule,
   ],
   controllers: [AppController],
   providers: [
